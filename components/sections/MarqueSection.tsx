13b454ea54c0b323e4d8f4a073efb55695ba780f
Shazzad998 <shazzadulislam998@gmail.com>
2024-11-02 12:46:26 +0600

    framer motion install

--- components/sections/MarqueSection.tsx
+++ components/sections/MarqueSection.tsx
@@ -1,10 +1,27 @@
+'use client'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 type MarqueSectionProps = {}
 
 const MarqueSection = (props: MarqueSectionProps) => {
   return (
-    <div>MarqueSection</div>
+    <div>
+      <motion.div
+        animate={{
+          translateX: "-50%",
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 10,
+          ease: "linear",
+          repeatType: "loop",
+        }}
+        className="flex gap-6 pr-6"
+      >
+        <div>asdfasdf</div>
+      </motion.div>
+    </div>
   )
 }
 
