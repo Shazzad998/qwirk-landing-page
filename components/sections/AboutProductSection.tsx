ecfcf7c8971d032fc23b32cac8377d33440a44a6
Shazzad998 <shazzadulislam998@gmail.com>
2024-11-03 19:54:21 +0600

    about and marque section

--- components/sections/AboutProductSection.tsx
+++ components/sections/AboutProductSection.tsx
@@ -1,10 +1,84 @@
+import Image from "next/image";
 
-type AboutProductSectionProps = {}
+type AboutProductSectionProps = {};
 
 const AboutProductSection = (props: AboutProductSectionProps) => {
   return (
-    <div>AboutProductSection</div>
-  )
-}
+    <div className=" container md:px-20 mx-auto space-y-4 md:space-y-8">
+      <div className=" space-y-2 md:space-y-4 max-w-4xl mx-auto text-center">
+        <h2 className=" text-3xl lg:text-4xl 2xl:text-5xl font-dosis font-bold"> Key Features to boost your productivity</h2>
+        <p className=" text-sm lg:text-base text-foreground/70">
+          Explore the essential tools designed to streamline your workglow,
+          enhance team collaboration, and ensure your projects run smoothly from
+          start to finish.
+        </p>
+      </div>
+      <div className=" grid sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-6xl mx-auto">
+        <div className=" flex flex-col sm:shrink-0 max-w-96 sm:max-w-[unset] mx-auto bg-green-500/10 rounded-md">
+          <div className=" p-6 space-y-2">
+            <span className=" w-8 h-8 flex items-center justify-center rounded-md bg-green-700 text-green-100">
+              {" "}
+              01
+            </span>
+            <h3 className=" text-2xl font-semibold">To-do List</h3>
+            <p className=" text-xs text-foreground/80">
+              Organize your daily tasks efforlessly with out intuatuve to-do
+              list. Stay focused and prioritize what matters most achiving the goal
+            </p>
+          </div>
+          <div className=" relative w-full aspect-[4/3]">
+            <Image
+              src="/images/task-feature.png"
+              className=" object-cover"
+              alt="Task Feature"
+              fill
+            />
+          </div>
+        </div>
+        <div className=" flex flex-col sm:shrink-0 max-w-96 sm:max-w-[unset] mx-auto bg-rose-500/10 rounded-md">
+          <div className=" p-6 space-y-2">
+            <span className=" w-8 h-8 flex items-center justify-center rounded-md bg-rose-700 text-rose-100">
+              {" "}
+              02
+            </span>
+            <h3 className=" text-2xl font-semibold">Team Member Tracking</h3>
+            <p className=" text-xs text-foreground/80">
+              Easily Track your team member&apos;s progress and stay connected. Ensure everyone is engaged and working towards shared goals.
+            </p>
+          </div>
+          <div className=" relative w-full aspect-[4/3]">
+            <Image
+              src="/images/member-feature.png"
+              className=" object-cover"
+              alt="Member Feature"
+              fill
+            />
+          </div>
+        </div>
+        <div className=" flex flex-col sm:shrink-0 max-w-96 sm:max-w-[unset] mx-auto bg-yellow-500/10 rounded-md">
+          <div className=" p-6 space-y-2">
+            <span className=" w-8 h-8 flex items-center justify-center rounded-md bg-yellow-700 text-yellow-100">
+              {" "}
+              03
+            </span>
+            <h3 className=" text-2xl font-semibold">Project Tracking</h3>
+            <p className=" text-xs text-foreground/80">
+              Monitor project timelines and milestones in real-time. keep projects on track and meet your deadlines with confidence.
+            </p>
+          </div>
+          <div className=" relative w-full aspect-[4/3]">
+            <Image
+              src="/images/project-feature.png"
+              className=" object-cover"
+              alt="Project Feature"
+              fill
+            />
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
 
-export default AboutProductSection
+export default AboutProductSection;
--- components/sections/MarqueSection.tsx
+++ components/sections/MarqueSection.tsx
@@ -16,34 +16,42 @@ const images = [
   "/images/Plain.svg",
 ];
 
-const MarqueeSection = () => {
+const imageWidth = 112 + 48;
+const MarqueComponent = ({ direction }: { direction: string }) => {
   const x = useMotionValue(0);
-  const imageWidth = 112 + 48; // Width of each image (adjust based on actual size + gap)
   const totalWidth = imageWidth * images.length;
 
   useAnimationFrame((_, delta) => {
-    // Move the slider leftward continuously, and reset `x` when it reaches `-totalWidth`
-    const newX = x.get() - delta * 0.1;
-    x.set(newX <= -totalWidth ? 0 : newX);
+    const moveSpeed = delta * 0.04; // Adjust speed multiplier as needed
+    const newX = direction === "left" ? x.get() - moveSpeed : x.get() + moveSpeed;
+    x.set(newX <= -totalWidth ? 0 : newX >= 0 ? -totalWidth : newX);
   });
 
+  
   return (
-    <div className="overflow-hidden w-full flex justify-center items-center py-10 md:py-14 lg:py-20 xl:py-24">
-      <motion.div className="flex" style={{ x }}>
-        {images
-          .concat(images)
-          .concat(images)
-          .map((src, index) => (
-            <div className="relative w-28 h-12 flex-shrink-0 mx-6" key={index}>
-              <Image
-                src={src}
-                alt={`Slide ${index}`}
-                fill
-                className="w-full h-full"
-              />
-            </div>
-          ))}
-      </motion.div>
+    <motion.div className="flex" style={{ x }}>
+      {images
+        .concat(images)
+        .concat(images)
+        .map((src, index) => (
+          <div className="relative w-28 h-12 flex-shrink-0 mx-6" key={index}>
+            <Image
+              src={src}
+              alt={`Slide ${index}`}
+              fill
+              className="w-full h-full"
+            />
+          </div>
+        ))}
+    </motion.div>
+  );
+};
+
+const MarqueeSection = () => {
+  return (
+    <div className="overflow-hidden w-full flex flex-col gap-6 py-10 md:py-14 lg:py-20 xl:py-24">
+      <MarqueComponent direction="left" />
+      <MarqueComponent direction="right" />
     </div>
   );
 };
